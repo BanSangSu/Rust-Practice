2b45f1bfa8e56527626e7574cd1bee0645ddd981
BanSangSu <asdd3525@nate.com>
2024-11-06 03:10:54 +0900

    studying comprehensive rust 16

--- comprehensive-rust/2-type-system/standard-library-types-exercise/src/main.rs
+++ comprehensive-rust/2-type-system/standard-library-types-exercise/src/main.rs
@@ -104,10 +104,57 @@ fn vec() {
 }
 
 
+
+// https://google.github.io/comprehensive-rust/std-types/hashmap.html
+use std::collections::HashMap;
+
+fn hashmap(){
+    let mut page_counts = HashMap::new();
+    page_counts.insert("Adventures of Huckleberry Finn", 207);
+    page_counts.insert("Grimms' Fairy Tales", 751);
+    page_counts.insert("Pride and Prejudice", 303);
+
+    if !page_counts.contains_key("Les Misérables") {
+        println!(
+            "We know about {} books, but not Les Misérables.",
+            page_counts.len()
+        );
+    }
+
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        match page_counts.get(book) {
+            Some(count) => println!("{book}: {count} pages"),
+            None => println!("{book} is unknown."),
+        }
+    }
+
+    for book in ["Pride and Prejudice", "Alice's Adventure in Wonderland"] {
+        let page_count: &mut i32 = page_counts.entry(book).or_insert(0);
+        *page_count += 1;
+    }
+
+    println!("{page_counts:#?}");
+    
+    let pc1 = page_counts
+        .get("Harry Potter and the Sorcerer's Stone")
+        .unwrap_or(&336);
+    println!("{pc1}");
+
+    let pc2 = page_counts
+        .entry("The Hunger Games")
+        .or_insert(374);
+    println!("{pc2}");
+    // println!("{pc1}, {pc2}"); error, mutalbe borrow occurs with immutable borrow later used.
+
+
+}
+
+
 fn main() {
     // documentation();
     // option();
     // result();
-    string();
-    vec();
+    // string();
+    // vec();
+    hashmap();
 }
