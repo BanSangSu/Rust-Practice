698445ec64576de25820f51e40fd0dbff64813b9
BanSangSu <asdd3525@nate.com>
2024-11-09 03:38:27 +0900

    studying comprehensive rust 19

--- comprehensive-rust/2-type-system/standard-library-types-exercise/src/main.rs
+++ comprehensive-rust/2-type-system/standard-library-types-exercise/src/main.rs
@@ -156,11 +156,68 @@ fn hashmap(){
 }
 
 
+
+
+//
+// https://google.github.io/comprehensive-rust/std-types/exercise.html
+// Exercise: Counter
+
+// use std::collections::HashMap;
+use std::hash::Hash;
+
+/// Counter counts the number of times each value of type T has been seen.
+struct Counter<T> {
+    values: HashMap<T, u64>,
+}
+
+impl<T: Eq + Hash> Counter<T> {
+    /// Create a new Counter.
+    fn new() -> Self {
+        Counter {
+            values: HashMap::new(),
+        }
+    }
+
+    /// Count an occurrence of the given value.
+    fn count(&mut self, value: T) {
+        *self.values.entry(value).or_default() += 1;
+    }
+
+    /// Return the number of times the given value has been seen.
+    fn times_seen(&self, value: T) -> u64 {
+        self.values.get(&value).copied().unwrap_or_default()
+    }
+}
+
+fn exercise_counter(){
+    let mut ctr = Counter::new();
+    ctr.count(13);
+    ctr.count(14);
+    ctr.count(16);
+    ctr.count(14);
+    ctr.count(14);
+    ctr.count(11);
+
+    for i in 10..20 {
+        println!("saw {} values equal to {}", ctr.times_seen(i), i);
+    }
+
+    let mut strctr = Counter::new();
+    strctr.count("apple");
+    strctr.count("orange");
+    strctr.count("apple");
+    println!("got {} apples", strctr.times_seen("apple"));
+}
+
+
 fn main() {
+    // Exercise: Counter
+    exercise_counter();
+
     // documentation();
     // option();
     // result();
     // string();
     // vec();
-    hashmap();
+    // hashmap();
 }
