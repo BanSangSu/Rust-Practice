aa27ff7b8c55e659850f10017ba752fdef15406a
BanSangSu <asdd3525@nate.com>
2024-11-10 04:21:32 +0900

    studying comprehensive rust 20

---
+++ comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
@@ -0,0 +1,48 @@
+// Agenda https://google.github.io/comprehensive-rust/std-traits.html
+
+
+
+// Comparisons https://google.github.io/comprehensive-rust/std-traits/comparisons.html
+fn comparisions() {
+    struct Key {
+        id:u32,
+        metadata: Option<String>,
+    }
+    // impl PartialEq for Key {
+    //     fn eq(&self, other: &Self) -> bool {
+    //         self.id == other.id
+    //     }
+    // } 
+    // Same above
+    impl PartialEq <u32> for Key {
+        fn eq(&self, other: &u32) -> bool {
+            self.id == *other
+        }
+    }
+
+    use std::cmp::Ordering;
+    #[derive(Eq, PartialEq)]
+    struct Citation {
+        author: String,
+        year: u32,
+    }
+    impl PartialOrd for Citation {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            match self.author.partial_cmp(&other.author) {
+                Some(Ordering::Equal) => self.year.partial_cmp(&other.year),
+                author_ord => author_ord,
+            }
+        }
+    }
+
+    let key1 = Key{ id:10, metadata: Some(String::from("B"))};
+    let key2 = Key{ id:10, metadata: Some(String::from("B"))};
+    let citation1 = Citation{ author: String::from("good"), year: 12};
+    let citation2 = Citation{ author: String::from("good"), year: 12};
+    println!("{}", key1.eq(&key2.id));
+    println!("{:?}", citation1.partial_cmp(&citation2));
+}
+
+fn main() {
+    comparisions();
+}
