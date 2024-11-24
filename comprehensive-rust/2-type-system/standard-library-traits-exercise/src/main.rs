9c2f2e341d6725bf29035a234cdb1398df690ce6
BanSangSu <asdd3525@nate.com>
2024-11-25 05:40:33 +0900

    studying comprehensive rust 35

--- comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
+++ comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
@@ -89,6 +89,8 @@ fn from_and_into() {
 fn casting() {
     let value: i64 = 1000;
     println!("as u16: {}", value as u16);
+    println!("as i16: {}", value as i16);
+    println!("as u8: {}", value as u8);
 }
 
 fn main() {
