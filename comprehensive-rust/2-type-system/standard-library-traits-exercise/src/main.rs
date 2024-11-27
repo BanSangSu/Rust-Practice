dbaa21cae1e7ee7c3c9d313f23e9016b58b5b34f
BanSangSu <asdd3525@nate.com>
2024-11-27 16:03:08 +0900

    studying comprehensive rust 38

--- comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
+++ comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
@@ -158,6 +158,39 @@ fn default_trait() {
     println!("{:#?}", nothing.unwrap_or_default());
 }
 
+
+// Closures https://google.github.io/comprehensive-rust/std-traits/closures.html
+fn closures() {
+    fn apply_and_log(func: impl FnOnce(i32) -> i32, func_name: &str, input: i32) {
+        println!("Calling {func_name}({input}): {}", func(input))
+    }
+
+    let n = 3;
+    let add_3 = |x| x + n;
+    apply_and_log(&add_3, "add_3", 10);
+    apply_and_log(&add_3, "add_3", 20);
+
+    let mut v = Vec::new();
+    let mut accumulate = |x: i32| {
+        v.push(x);
+        v.iter().sum::<i32>()
+    };
+    apply_and_log(&mut accumulate, "accumulate", 4);
+    apply_and_log(&mut accumulate, "accumulate", 5);
+
+    let multiply_sum = |x| x * v.into_iter().sum::<i32>();
+    apply_and_log(multiply_sum, "multiply_sum", 3);
+
+
+    fn make_greeter(prefix: String) -> impl Fn(&str) {
+        return move |name| println!("{} {}", prefix, name);
+    }
+
+    let hi = make_greeter("Hi".to_string());
+    hi("Greg");
+}
+
+
 fn main() {
     comparisions();
     operators();
@@ -166,4 +199,5 @@ fn main() {
     read();
     write();
     default_trait();
+    closures();
 }
