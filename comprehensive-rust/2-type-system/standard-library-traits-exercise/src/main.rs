35318770cb0e2fe7cee0ef4f608a558b7134751a
BanSangSu <asdd3525@nate.com>
2024-11-28 23:06:01 +0900

    studying comprehensive rust 39

--- comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
+++ comprehensive-rust/2-type-system/standard-library-traits-exercise/src/main.rs
@@ -191,13 +191,78 @@ fn closures() {
 }
 
 
+
+
+// Exercise: ROT13 https://google.github.io/comprehensive-rust/std-traits/exercise.html#exercise-rot13
+
+// use std::io::Read;
+
+struct RotDecoder<R: Read> {
+    input: R,
+    rot: u8,
+}
+
+impl <R: Read> Read for RotDecoder<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let size = self.input.read(buf)?;
+        for b in &mut buf[..size] {
+            if b.is_ascii_alphabetic() {
+                let base = if b.is_ascii_uppercase() { 'A' } else { 'a' } as u8;
+                *b = (*b - base + self.rot) % 26 + base;
+            }
+        }
+        Ok(size)
+    }
+}
+
+fn exercise_rot13() {
+
+    let mut rot =
+        RotDecoder { input: "Gb trg gb gur bgure fvqr!".as_bytes(), rot: 13 };
+    let mut result = String::new();
+    rot.read_to_string(&mut result).unwrap();
+    println!("{}", result);
+        
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn joke() {
+        let mut rot =
+            RotDecoder { input: "Gb trg gb gur bgure fvqr!".as_bytes(), rot: 13 };
+        let mut result = String::new();
+        rot.read_to_string(&mut result).unwrap();
+        assert_eq!(&result, "To get to the other side!");
+    }
+
+    #[test]
+    fn binary() {
+        let input: Vec<u8> = (0..=255u8).collect();
+        let mut rot = RotDecoder::<&[u8]> { input: input.as_ref(), rot: 13 };
+        let mut buf = [0u8; 256];
+        assert_eq!(rot.read(&mut buf).unwrap(), 256);
+        for i in 0..=255 {
+            if input[i] != buf[i] {
+                assert!(input[i].is_ascii_alphabetic());
+                assert!(buf[i].is_ascii_alphabetic());
+            }
+        }
+    }
+}
+
+
 fn main() {
-    comparisions();
-    operators();
-    from_and_into();
-    casting();
-    read();
-    write();
-    default_trait();
-    closures();
+    exercise_rot13();
+
+    // comparisions();
+    // operators();
+    // from_and_into();
+    // casting();
+    // read();
+    // write();
+    // default_trait();
+    // closures();
 }
