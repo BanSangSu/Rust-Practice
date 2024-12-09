098bf5fc1760cf00c0c8b7775e1a3907f32e45da
BanSangSu <asdd3525@nate.com>
2024-12-10 02:52:10 +0900

    studying comprehensive rust 50

--- comprehensive-rust/3-memory-safety-and-smart-pointers/smart-pointers-exercise/src/main.rs
+++ comprehensive-rust/3-memory-safety-and-smart-pointers/smart-pointers-exercise/src/main.rs
@@ -29,7 +29,51 @@ fn reference_counted() {
 }
 
 
+// Owned Trait Objects https://google.github.io/comprehensive-rust/smart-pointers/trait-objects.html
+fn owned_trait_objects() {
+    struct Dog {
+        name: String,
+        age: i8,
+    }
+    struct Cat {
+        lives: i8,
+    }
+
+    trait Pet {
+        fn talk(&self) -> String;
+    }
+
+    impl Pet for Dog {
+        fn talk(&self) -> String {
+            format!("Woof, my name is {}!", self.name)
+        }
+    }
+
+    impl Pet for Cat {
+        fn talk(&self) -> String {
+            String::from("Miau!")
+        }
+    }
+
+    let pets: Vec<Box<dyn Pet>> = vec! [
+        Box::new(Cat { lives: 9 }),
+        Box::new(Dog { name: String::from("Fido"), age: 5 }),
+    ];
+
+    for pet in pets {
+        println!("Hello, who are you? {}", pet.talk());
+    }
+
+    println!("{} {}", std::mem::size_of::<Dog>(), std::mem::size_of::<Cat>());
+    println!("{} {}", std::mem::size_of::<&Dog>(), std::mem::size_of::<&Cat>());
+    println!("{}", std::mem::size_of::<&dyn Pet>());
+    println!("{}", std::mem::size_of::<Box<dyn Pet>>());
+}
+
+
+
 fn main() {
     box_type();
     reference_counted();
+    owned_trait_objects();
 }
