8d96651c5bc895f4b2689b20cf33aa56f48a3bba
BanSangSu <asdd3525@nate.com>
2024-12-19 03:36:27 +0900

    studying comprehensive rust 59

--- comprehensive-rust/3-memory-safety-and-smart-pointers/borrowing-exercise/src/main.rs
+++ comprehensive-rust/3-memory-safety-and-smart-pointers/borrowing-exercise/src/main.rs
@@ -26,14 +26,23 @@ fn borrowing_checking() {
     // };
     // println!("x: {x_ref}");
 
+    // let mut a: i32 = 10;
+    // let b: &i32 = &a;
+    // // {
+    // //     let c: &mut i32 = &mut a; // cause error: mutable borrow occurs here
+    // //     *c = 20;
+    // // }
+    // println!("a: {a}");
+    // println!("b: {b}");
+
     let mut a: i32 = 10;
     let b: &i32 = &a;
-    // {
-    //     let c: &mut i32 = &mut a; // cause error: mutable borrow occurs here
-    //     *c = 20;
-    // }
-    println!("a: {a}");
     println!("b: {b}");
+    {
+        let c: &mut i32 = &mut a;
+        *c = 20;
+    }
+    println!("a: {a}");
 }
 
 
