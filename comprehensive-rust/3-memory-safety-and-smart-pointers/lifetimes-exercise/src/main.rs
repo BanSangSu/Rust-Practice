ab56989eecbdbd7b48c9fc35d07b91aac8bd685c
BanSangSu <asdd3525@nate.com>
2024-12-24 14:09:28 +0900

    studying comprehensive rust 65

---
+++ comprehensive-rust/3-memory-safety-and-smart-pointers/lifetimes-exercise/src/main.rs
@@ -0,0 +1,25 @@
+// Lifetime Annotations https://google.github.io/comprehensive-rust/lifetimes/lifetime-annotations.html
+
+
+fn lifetime_annotations() {
+    #[derive(Debug)]
+    struct Point(i32, i32);
+
+    fn left_most<'a>(p1: &'a Point, p2: &'a Point) -> &'a Point {
+        if p1.0 < p2.0 {
+            p1
+        } else {
+            p2
+        }
+    }
+
+    let p1: Point = Point(10, 10);
+    let p2: Point = Point(20, 20);
+    let p3 = left_most(&p1, &p2); // What is the lifetime of p3?
+    println!("p3: {p3:?}");
+}
+
+
+fn main() {
+    lifetime_annotations();
+}
