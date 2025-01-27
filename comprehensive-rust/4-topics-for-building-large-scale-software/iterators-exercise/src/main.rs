3dcdcd4c81b5ce43d3748c76878a9e03cb006ca8
BanSangSu <asdd3525@nate.com>
2025-01-28 00:18:30 +0900

    studying comprehensive rust 101

--- comprehensive-rust/4-topics-for-building-large-scale-software/iterators-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/iterators-exercise/src/main.rs
@@ -49,7 +49,11 @@ fn iterator_trait() {
 // Iterator Helper Methods https://google.github.io/comprehensive-rust/iterators/helpers.html
 fn iterator_helper_methods() {
     let result: i32 = (1..=10) // Create a range from 1 to 10
-    
+        .filter(|x| x % 2 == 0) // Keep only even numbers
+        .map(|x| x * x) // Square each number
+        .sum(); // Sum up all the squared numbers
+
+    println!("The sum of squares of even numbers from 1 to 10 is: {}", result);
 }
 
 
