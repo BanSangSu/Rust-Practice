ca8c8f53c459abf3144004a0d95daa37a10eb298
BanSangSu <asdd3525@nate.com>
2025-02-03 01:27:00 +0900

    studying comprehensive rust 107

--- comprehensive-rust/4-topics-for-building-large-scale-software/iterators-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/iterators-exercise/src/main.rs
@@ -69,21 +69,81 @@ fn into_iterator() {
         y_coords: Vec<u32>,
     }
 
-    impl IntoIterator for Grid {
+    // impl IntoIterator for Grid {
+    //     type Item = (u32, u32);
+    //     type IntoIter = GridIter;
+    //     fn into_iter(self) -> GridIter {
+    //         GridIter { grid: self, i: 0, j: 0 }
+    //     }
+    // }
+    // struct GridIter {
+    //     grid: Grid,
+    //     i: usize,
+    //     j: usize,
+    // }  
+    
+    // impl Iterator for GridIter {
+    //     type Item = (u32, u32);
+
+    //     fn next(&mut self) -> Option<(u32, u32)> {
+    //         if self.i >= self.grid.x_coords.len() {
+    //             self.i = 0;
+    //             self.j += 1;
+    //             if self.j >= self.grid.y_coords.len() {
+    //                 return None;
+    //             }
+    //         }
+    //         let res = Some((self.grid.x_coords[self.i], self.grid.y_coords[self.j]));
+    //         self.i += 1;
+    //         res   
+    //     }
+    // }
+
+    /// reference method
+    impl<'a> IntoIterator for &'a Grid { 
         type Item = (u32, u32);
-        type IntoIter = GridIter;
-        fn into_iter(self) -> GridIter {
-            GridIter { grid: self, i: 0, j: 0 }
+        type IntoIter = GridRefIter<'a>;
+        fn into_iter(self) -> GridRefIter<'a> {
+            GridRefIter { grid: self, i: 0, j: 0 }
+        }
+    }
+    
+    struct GridRefIter<'a> {
+        grid: &'a Grid,
+        i: usize,
+        j: usize,
+    }
+
+    impl<'a> Iterator for GridRefIter<'a> {
+        type Item = (u32, u32);
+
+        fn next(&mut self) -> Option<(u32, u32)> {
+            if self.i >= self.grid.x_coords.len() {
+                self.i = 0;
+                self.j += 1;
+                if self.j >= self.grid.y_coords.len() {
+                    return None;
+                }
+            }
+            let res = Some((self.grid.x_coords[self.i], self.grid.y_coords[self.j]));
+            self.i += 1;
+            res   
         }
     }
 
-    struct GridIter {
-        grid; Grid,
-        i: usize,
-        j: usize,
-    }    
-    
-    
+
+    let grid = Grid { x_coords: vec![3, 5, 7, 9], y_coords: vec![10, 20, 30, 40] };
+    // for (x, y) in grid {
+    //     println!("point = {x}, {y}");
+    // }
+
+    for (x, y) in &grid {
+        println!("point = {x}, {y}");
+    }
+    println!("One more!");
+    for (x, y) in &grid { // Reference method makes it possible.
+        println!("point = {x}, {y}");
+    }
 }
 
 fn main() {
