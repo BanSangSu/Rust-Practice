0cbd0eb275062e4046018e6ad080fe4812a55caf
BanSangSu <asdd3525@nate.com>
2025-02-16 18:28:14 +0900

    studying comprehensive rust 123

--- comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
@@ -20,6 +20,31 @@ fn panics() {
 // [profile.dev]
 // panic = 'abort'
 
+
+// Result https://google.github.io/comprehensive-rust/error-handling/result.html
+use std::fs::File;
+use std::io::Read;
+
+fn result() {
+    let file: Result<File, std::io::Error> = File::open("diary.txt");
+    match file {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            if let Ok(bytes) = file.read_to_string(&mut contents) {
+                println!("Dear diary: {contents} ({bytes} bytes)");
+            } else {
+                println!("Could not read file content");
+            }
+        }
+        Err(err) => {
+            println!("The diary could not be opened: {err}");
+        }
+    }
+}
+
+
+
 fn main() {
-    panics();
+    result();
+    // panics();
 }
