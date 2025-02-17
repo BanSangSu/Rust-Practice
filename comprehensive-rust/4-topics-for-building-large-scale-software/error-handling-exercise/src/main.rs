e3a90770c1a3a7b1afa631a506a7a8e302c1e3f9
BanSangSu <asdd3525@nate.com>
2025-02-17 18:45:31 +0900

    studying comprehensive rust 124

--- comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
@@ -22,29 +22,56 @@ fn panics() {
 
 
 // Result https://google.github.io/comprehensive-rust/error-handling/result.html
-use std::fs::File;
-use std::io::Read;
+// use std::fs::File;
+// use std::io::Read;
 
-fn result() {
-    let file: Result<File, std::io::Error> = File::open("diary.txt");
-    match file {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            if let Ok(bytes) = file.read_to_string(&mut contents) {
-                println!("Dear diary: {contents} ({bytes} bytes)");
-            } else {
-                println!("Could not read file content");
-            }
+// fn result() {
+//     let file: Result<File, std::io::Error> = File::open("diary.txt");
+//     match file {
+//         Ok(mut file) => {
+//             let mut contents = String::new();
+//             if let Ok(bytes) = file.read_to_string(&mut contents) {
+//                 println!("Dear diary: {contents} ({bytes} bytes)");
+//             } else {
+//                 println!("Could not read file content");
+//             }
+//         }
+//         Err(err) => {
+//             println!("The diary could not be opened: {err}");
+//         }
+//     }
+// }
+
+// Try Operator https://google.github.io/comprehensive-rust/error-handling/try.html
+use std::io::Read;
+use std::{fs, io};
+
+fn try_operator() {
+    fn read_username(path: &str) -> Result<String, io::Error> {
+        let username_file_result = fs::File::open(path);
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(err) => return Err(err),
+        };
+
+        let mut username = String::new();
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(err) => Err(err),
         }
-        Err(err) => {
-            println!("The diary could not be opened: {err}");
-        }
+
     }
+
+    //  fs::write("config.dat", "alice").unwrap();
+     let username = read_username("config.dat");
+     println!("username or error: {username:?}");
+
 }
 
 
 
 fn main() {
-    result();
+    try_operator();
+    // result();
     // panics();
 }
