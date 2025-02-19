e7069e84ccd2e95773be06264fda16c4caf5e3ea
BanSangSu <asdd3525@nate.com>
2025-02-19 18:47:29 +0900

    studying comprehensive rust 126

--- comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
@@ -48,21 +48,26 @@ use std::{fs, io};
 
 fn try_operator() {
     fn read_username(path: &str) -> Result<String, io::Error> {
-        let username_file_result = fs::File::open(path);
-        let mut username_file = match username_file_result {
-            Ok(file) => file,
-            Err(err) => return Err(err),
-        };
+        // let username_file_result = fs::File::open(path);
+        // let mut username_file = match username_file_result {
+        //     Ok(file) => file,
+        //     Err(err) => return Err(err),
+        // };
 
+        // let mut username = String::new();
+        // match username_file.read_to_string(&mut username) {
+        //     Ok(_) => Ok(username),
+        //     Err(err) => Err(err),
+        // }
+
+        // Simplified version
         let mut username = String::new();
-        match username_file.read_to_string(&mut username) {
-            Ok(_) => Ok(username),
-            Err(err) => Err(err),
-        }
-
+        fs::File::open(path)?.read_to_string(&mut username)?;
+        Ok(username)
+        
     }
 
-     fs::write("config.dat", "alice").unwrap();
+    //  fs::write("config.dat", "alice").unwrap();
      let username = read_username("config.dat");
      println!("username or error: {username:?}");
 
