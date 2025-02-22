678dd24e1579604ff5cbb8b73019e7aa02742d58
BanSangSu <asdd3525@nate.com>
2025-02-22 19:26:18 +0900

    studying comprehensive rust 129

--- comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/error-handling-exercise/src/main.rs
@@ -126,12 +126,16 @@ use std::io::Read;
 fn dynamic_error_types() {
     fn read_count(path: &str) -> Result<i32, Box<dyn Error>> {
         let mut count_str = String::new();
-        fs::File:open(path)?.read_to_string(&mut count_str)?;
+        fs::File::open(path)?.read_to_string(&mut count_str)?;
         let count:i32 = count_str.parse()?;
         Ok(count)
     }
 
-    
+    fs::write("count.dat", "1i3").unwrap();
+    match read_count("count.dat") {
+        Ok(count) => println!("Count: {count}"),
+        Err(err) => println!("Error: {err}"),
+    }
 }
 
 fn main() {
