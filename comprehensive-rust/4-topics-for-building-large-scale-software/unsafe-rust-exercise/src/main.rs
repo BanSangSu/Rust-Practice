8796f9989c275764ec3cd5ff267dd2cbe8066973
BanSangSu <asdd3525@nate.com>
2025-03-13 19:57:45 +0900

    studying comprehensive rust 148

--- comprehensive-rust/4-topics-for-building-large-scale-software/unsafe-rust-exercise/src/main.rs
+++ comprehensive-rust/4-topics-for-building-large-scale-software/unsafe-rust-exercise/src/main.rs
@@ -114,9 +114,10 @@ fn unsafe_rust_functions() {
 }
 
 // Unsafe External Functions https://google.github.io/comprehensive-rust/unsafe-rust/unsafe-functions/extern-c.html
-use std::ffi::c_char;
 
 fn unsafe_external_functions () {
+    use std::ffi::c_char;
+    
     unsafe extern "C" {
         // `abs` doesn't deal with pointers and doesn't have any safety requirements.
         safe fn abs(input: i32) -> i32;
@@ -184,9 +185,111 @@ fn implementing_unsafe_traits() {
 }
 
 
+//////
+// Exercise: FFI Wrapper
+// Safe FFI Wrapper https://google.github.io/comprehensive-rust/unsafe-rust/exercise.html
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_imports, unused_variables, dead_code)]
+
+mod ffi {
+    use std::os::raw::{c_char, c_int};
+    #[cfg(not(target_os = "macos"))]
+    use std::os::raw::{c_long, c_uchar, c_ulong, c_ushort};
+
+    // Opaque type. See https://doc.rust-lang.org/nomicon/ffi.html.
+    #[repr(C)]
+    pub struct DIR {
+        _data: [u8; 0],
+        _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+    }
+
+    // Layout according to the Linux man page for readdir(3), where ino_t and
+    // off_t are resolved according to the definitions in
+    // /usr/include/x86_64-linux-gnu/{sys/types.h, bits/typesizes.h}.
+    #[cfg(not(target_os = "macos"))]
+    #[repr(C)]
+    pub struct dirent {
+        pub d_ino: c_ulong,
+        pub d_off: c_long,
+        pub d_reclen: c_ushort,
+        pub d_type: c_uchar,
+        pub d_name: [c_char; 256],
+    }
+
+    // Layout according to the macOS man page for dir(5).
+    #[cfg(all(target_os = "macos"))]
+    #[repr(C)]
+    pub struct dirent {
+        pub d_fileno: u64,
+        pub d_seekoff: u64,
+        pub d_reclen: u16,
+        pub d_namlen: u16,
+        pub d_type: u8,
+        pub d_name: [c_char; 1024],
+    }
+
+    unsafe extern "C" {
+        pub unsafe fn opendir(s: *const c_char) -> *mut DIR;
+
+        #[cfg(not(all(target_os = "macos", target_arch = "x86_64")))]
+        pub unsafe fn readdir(s: *mut DIR) -> *const dirent;
+
+        // See https://github.com/rust-lang/libc/issues/414 and the section on
+        // _DARWIN_FEATURE_64_BIT_INODE in the macOS man page for stat(2).
+        //
+        // "Platforms that existed before these updates were available" refers
+        // to macOS (as opposed to iOS / wearOS / etc.) on Intel and PowerPC.
+        #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+        #[link_name = "readdir$INODE64"]
+        pub unsafe fn readdir(s: *mut DIR) -> *const dirent;
+
+        pub unsafe fn closedir(s: *mut DIR) -> c_int;
+    }
+}
+
+use std::ffi::{CStr, CString, OsStr, OsString};
+use std::os::unix::ffi::OsStrExt;
+
+#[derive(Debug)]
+struct DirectoryIterator {
+    path: CString,
+    dir: *mut ffi::DIR,
+}
+
+impl DirectoryIterator {
+    fn new(path: &str) -> Result<DirectoryIterator, String> {
+        // Call opendir and return a Ok value if that worked,
+        // otherwise return Err with a message.
+        todo!()
+    }
+}
+
+impl Iterator for DirectoryIterator {
+    type Item = OsString;
+    fn next(&mut self) -> Option<OsString> {
+        // Keep calling readdir until we get a NULL pointer back.
+        todo!()
+    }
+}
+
+impl Drop for DirectoryIterator {
+    fn drop(&mut self) {
+        // Call closedir as needed.
+        todo!()
+    }
+}
+
+fn safe_ffi_wrapper_exercise() -> Result<(), String> {
+    let iter = DirectoryIterator::new(".")?;
+    println!("files: {:#?}", iter.collect::<Vec<_>>());
+    Ok(())
+}
+
+
 fn main() {
+    safe_ffi_wrapper_exercise();
         
-    implementing_unsafe_traits();
+    // implementing_unsafe_traits();
     // calling_unsafe_functions();
     // unsafe_external_functions();
     // unsafe_rust_functions();
